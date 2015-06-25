9c46cf3b79c69d017ad7c4e7d53e58ced8f7f90a
Manuel Rodriguez Morales <marodriguez@marodriguez.com>
2015-06-25 20:55:59 +0200

    publi

--- Ziip/Ziip/Define.h
+++ Ziip/Ziip/Define.h
@@ -12,19 +12,21 @@
 #ifndef d3_Define_h
 #define d3_Define_h
 
-//#define CONEXION_URL @"http://ziip.marodriguez.com/api/"
+#define CONEXION_URL @"http://ziip.marodriguez.com/api/"
 //#define CONEXION_URL @"http://192.168.1.129:8000/api/"
-#define CONEXION_URL @"http://127.0.0.1:8000/api/"
+//#define CONEXION_URL @"http://127.0.0.1:8000/api/"
 
 
 
 
 #define IMAGENES_URL @"http://ziip.marodriguez.com/"
 #define PUBLICIDAD_URL @"http://relax.marodriguez.com/api/"
-#define IMAGENES_PUBLI_URL @"http://relax.marodriguez.com"
+#define IMAGENES_PUBLI_URL @"http://relax.marodriguez.com/publicidad/"
 
 
 #define CHAT_URL @"ziip.marodriguez.com"
+//#define CHAT_URL @"localhost"
+
 
 
 #define TIPO_MENSAJE_BLOQUEAR = 4
