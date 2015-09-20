f63b2b236b0508171e080a1372574b7222b7a7ec
Manuel Rodriguez Morales <manuthema@MacBook-Pro-de-Manuel.local>
2015-09-20 18:03:28 +0200

    Cambio en notificaciones push

--- server_chat/config.js
+++ server_chat/config.js
@@ -5,17 +5,17 @@
 
 
 //Local
-var conf = { 
+var conf = {
     port: 8888,
     debug: false,
     dbPort: 6379,
     dbHost: 'localhost',
     dbOptions: {},
     mainroom: 'MainRoom',
-	mysqlHost:'213.251.185.53',
-	mysqlUser:'datting',
-	mysqlPass:'lqih4to7',
-	mysqlDatabase:'datting',
+    mysqlHost:'54.155.58.232',
+    mysqlUser:'ziip',
+    mysqlPass:'lkwh46w8u3f',
+    mysqlDatabase:'ziip',
 	imgFullPath:"/Users/marodriguez/git/ziip/server_chat/imagenes",
 	imgPublicPath:"img/chat/",
 	errorLog:"error.log",
